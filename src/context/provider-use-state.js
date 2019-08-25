65db07d6214bfdbc9555d0719c90322c17e98e10
Sameer Sarmah <sameerparansarmah@gmail.com>
2019-08-25 11:23:39 +0530

    adding usestate hook for global state

---
+++ src/context/provider-use-state.js
@@ -0,0 +1,58 @@
+import React, { useEffect ,useState} from 'react';
+import {CartService} from "./../services/cart-service.service";
+import {RecipeService} from "./../services/recipe.service";
+import {Context} from './context';
+import {shoppingReducer} from './../reducers/recipe-reducer';
+import {cartReducer} from './../reducers/shopping-cart-reducer';
+
+const initialShoppingState = {
+    recipes: [],
+    selectedRecipe: null
+};
+const initialCartState = {
+    recipesInCart:[]
+};
+const  recipeService=new RecipeService();
+
+
+export const Provider = (props)=> {
+
+  
+    useEffect(() => {
+        const recipes= recipeService.getRecipes();
+        const selectedRecipe=recipes[0];
+        setShoppingState({...shoppingState,selectedRecipe,recipes});
+    },[]);
+
+    const [shoppingState, setShoppingState ] = useState(initialShoppingState);
+    const [cartState, setCartState] = useState(initialCartState);
+    
+    const addToCart =() => {
+        if(!!shoppingState.selectedRecipe){
+          CartService.addToCart(shoppingState.selectedRecipe);
+          setCartState({...initialCartState,recipesInCart:CartService.getRecipesInCart()});
+        }
+    };
+    
+    const removeFromCart = (recipe) => {
+        CartService.removeFromCart(recipe.recipe_id);
+        setCartState({...initialCartState,recipesInCart:CartService.getRecipesInCart()});
+    };
+    
+    const getRecipe = (recipeID) => {
+       return recipeService.getRecipe(recipeID);
+    };
+    
+    const setSelectedRecipe = (selectedRecipe) => {
+        setShoppingState({...shoppingState,selectedRecipe});
+    };
+  
+    return (
+        <Context.Provider value={ {...shoppingState ,  ...cartState,
+        addToCart:addToCart,  removeFromCart:removeFromCart ,
+        getRecipe:getRecipe , setSelectedRecipe:setSelectedRecipe }}>
+          {props.children}
+        </Context.Provider>
+      );
+   
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Provider} from "./context/context";
+//import {Provider} from "./context/provider-class";
+import {Provider} from "./context/provider-use-state";
 
 ReactDOM.render(
   <Provider><App /></Provider>,
